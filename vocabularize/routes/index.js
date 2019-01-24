114e229385c270ec8be0f93113fecee8c492688f
mikioosaki <mikio.osaki@posteo.de>
2019-01-24 15:12:44 +0100

    extract rout to contoller

--- vocabularize/routes/index.js
+++ vocabularize/routes/index.js
@@ -1,4 +1,4 @@
-/* var express = require('express');
+var express = require('express');
 var router = express.Router();
 
 var textinput_controller = require('../controllers/textinputController');
@@ -22,34 +22,5 @@ router.get('/', textinput_controller.upload_get);
 
 // POST request for creating textinput.
 router.post('/', upload.single('file-to-upload'), textinput_controller.upload_post);
-
-module.exports = router;
-
-
- */
-var express = require('express');
-var router = express.Router();
-
-//multer object creation
-var multer = require('multer')
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
-
-var upload = multer({ storage: storage })
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.post('/', upload.single('imageupload'), function (req, res) {
-  res.send("File upload sucessfully.");
-});
 
 module.exports = router;
