6bd9217710cc2020cf5fa61998143dca5b8cb30a
mikioosaki <mikio.osaki@posteo.de>
2019-01-24 15:08:47 +0100

    setup multer

--- vocabularize/controllers/textinputController.js
+++ vocabularize/controllers/textinputController.js
@@ -1,21 +1,11 @@
+const express = require('express');
 
-// Display Category create form on GET.
-exports.textinput_create_get = function (req, res, next) {
+/* GET home page. */
+exports.upload_get = function (req, res, next) {
     res.render('index', { title: 'Extract vocabulary from text' });
 };
 
-// Handle Category create on POST.
-exports.textinput_create_post = [
-
-       // Process request after validation and sanitization.
-    (req, res, next) => {
-
-        // Extract the validation errors from a request.
-        const errors = validationResult(req);
-
-        // Create a category object with escaped and trimmed data.
-        var bathingspot = new Bathingspot(
-            { name: req.body.name }
-        );
-    }
-];
+// Handle POST.
+exports.upload_post = function (req, res) {
+    res.send("File upload sucessfully.");
+};
--- vocabularize/routes/index.js
+++ vocabularize/routes/index.js
@@ -1,15 +1,55 @@
-var express = require('express');
+/* var express = require('express');
 var router = express.Router();
 
 var textinput_controller = require('../controllers/textinputController');
 
 
+//multer object creation
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+var upload = multer({ storage: storage })
 
 // GET request for creating textinput. NOTE This must come before route for id (i.e. display textinput).
-router.get('/index', textinput_controller.textinput_create_get);
+router.get('/', textinput_controller.upload_get);
 
 // POST request for creating textinput.
-router.post('/index/create', textinput_controller.textinput_create_post);
+router.post('/', upload.single('file-to-upload'), textinput_controller.upload_post);
 
+module.exports = router;
+
+
+ */
+var express = require('express');
+var router = express.Router();
+
+//multer object creation
+var multer = require('multer')
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+var upload = multer({ storage: storage })
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', upload.single('imageupload'), function (req, res) {
+  res.send("File upload sucessfully.");
+});
 
 module.exports = router;
